86a7fab22db3ff75b8436959deef227e81944aea
Nithin Krishna <nithin.krishna@omniware.in>
2021-12-22 15:51:02 +0530

    Added:initial commit.

---
+++ resources/js/datatable.js
@@ -0,0 +1,6 @@
+require( 'datatables.net-bs5' )();
+require( 'datatables.net-buttons-bs5' )();
+require( 'datatables.net-datetime' )();
+require( 'datatables.net-fixedheader-bs5' )();
+require( 'datatables.net-searchbuilder-bs5' )();
+require( 'datatables.net-responsive-bs5' )();
---
+++ resources/views/appLayout.blade.php
@@ -0,0 +1,14 @@
+@extends('welcome')
+
+@push('css')
+
+@endpush
+
+@section('content')
+    <h1>hello World</h1>
+@endsection
+
+
+@push('js')
+
+@endpush
